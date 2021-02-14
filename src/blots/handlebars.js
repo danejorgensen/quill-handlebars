79404b079ebc40116f2d3e2db6df0b9d21561fc0
Dane Jorgensen <spam@danej.com>
2021-02-14 13:15:40 -0700

    remove attrs from handlebars class

--- src/blots/handlebars.js
+++ src/blots/handlebars.js
@@ -5,14 +5,15 @@ const Embed = Quill.import("blots/embed");
 class HandlebarsBlot extends Embed {
   static create(data) {
     const node = super.create();
+    node.innerHTML += `{{${data.value}}}`;
 /*
     const denotationChar = document.createElement("span");
     denotationChar.className = "ql-handlebars-denotation-char";
     denotationChar.innerHTML = data.denotationChar;
     node.appendChild(denotationChar);
-*/
-    node.innerHTML += `{{${data.value}}}`;
     return HandlebarsBlot.setDataValues(node, data);
+*/
+    return node;
   }
 
   static setDataValues(element, data) {
